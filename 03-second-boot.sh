4bc23ad9ac846733c6ed09c3e80c2e43517ad7a4
Thorsten Fischer <thorsten@froschi.org>
2012-08-20 13:56:35 +0200

    Fixes to RAID creation.
    Create RAID with 4 drives and 1 spare, rather than with 5 drives.
    Also, pipe the output of mdadm --examine --scan into mdadm.conf.
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- 03-second-boot.sh
+++ 03-second-boot.sh
@@ -8,10 +8,12 @@ done
 
 sleep 5
 
-mdadm --create /dev/md0 --level=10 --raid-devices=5 /dev/sd[b-f]1
+mdadm --create /dev/md0 --level=10 --raid-devices=4 --spare-devices=1 /dev/sd[b-f]1
 
 # TODO: Find a way to reliably wait for completion. Might grep /proc/mdstat
-# in a loop.
+# in a loop. The term to look for is 'resync'.
+
+mdadm --examine --scan >> /etc/mdadm/mdadm.conf
 
 pvcreate /dev/md0
 vgcreate data /dev/md0
